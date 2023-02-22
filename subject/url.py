9fd3ae35be315b075dfd87d6cafd224feb040247
ilyab <i-baykov@internet.ru>
2023-02-22 15:27:42 +0300

    Начальный коммит

---
+++ subject/url.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from subject import views as data_subject
+
+urlpatterns = [
+    path('<int:current_subject_int>', data_subject.choice_of_discipline_int),
+    path('<str:current_subject_str>', data_subject.choice_of_discipline_str, name="subject_url"),
+]
---
+++ subject/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+dict_subject = {
+    "system_analysis": "Лексин А.Ю.",
+    "spoken_foreign_language": " Зимакова Е.С.",
+    "political_science": "Ефимова С.А",
+    "fuzzy_sets": "Абрахин С.И.",
+}
+
+
+def choice_of_discipline_str(request, current_subject_str):
+    if dict_subject.get(current_subject_str):
+        return HttpResponse(f"Преподователь по {current_subject_str}  - {dict_subject[current_subject_str]}")
+    return HttpResponseNotFound(f"Нам очень жаль , но мы не знаем такую дисцилину ;с  {current_subject_str}")
+
+
+def choice_of_discipline_int(request, current_subject_int):
+    list_subject = list(dict_subject)
+    if len(list_subject) >= current_subject_int > 0:
+        return HttpResponseRedirect(f'{list_subject[current_subject_int - 1]}')
+    return HttpResponseNotFound(f"Введите число от 1 до 4 , вы ввели - {current_subject_int}")
